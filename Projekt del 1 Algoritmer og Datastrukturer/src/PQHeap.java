dd83d7b42a26d58a6511c31bb69e1c93a909536f
GingerDres <61565534+GingerDres@users.noreply.github.com>
2020-03-01 17:23:27 +0100

    Add files via upload

---
+++ Projekt del 1 Algoritmer og Datastrukturer/src/PQHeap.java
@@ -0,0 +1,16 @@
+import java.util.ArrayList;
+
+public abstract class PQHeap implements PQ {
+
+// @sdu logins
+// Deadline: 16 marts 2020 kl 12:00
+
+    // Constructor for making a new PQHeap
+    PQHeap() {
+        
+    }
+
+
+
+
+}
